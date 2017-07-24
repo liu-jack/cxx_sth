2a4e8a1bba5c353624886106ef4c476b611c01ad
Alexjandro <zhoulunhao@hotmail.com>
2017-07-24 13:39:58 +0800

    an example of TMP

--- template_meta_programming.h
+++ template_meta_programming.h
@@ -3,8 +3,21 @@
 #ifndef __TEMPLATE_META_PROGRAMMING__H
 #define __TEMPLATE_META_PROGRAMMING__H
 
+
+///No I'm wrong.Function template can't be partial specialization.
 namespace zhoulunhao
 {
+	template< size_t N>
+	struct Factorial
+	{
+		enum { value = N * Factorial<N-1>::value};
+	};
+	template<>
+	struct Factorial<0>
+	{
+		enum { value = 1};
+	};
+
 	template< typename T,size_t N>
 	T power()
 	{
